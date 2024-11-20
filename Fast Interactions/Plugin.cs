7156ddffc2173e753e7138c64b684459a2607ebc
V0ID-Whisperer <94946192+V0IDL355@users.noreply.github.com>
2024-11-21 01:45:01 +0200

    Initial push

---
+++ Fast Interactions/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using IAmFuture.UserInterface.GameplayMenu;
+
+namespace Fast_Interactions;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private static ManualLogSource _logger;
+
+    [HarmonyPatch(typeof(GUI_QteModalBase))]
+    class FasterTools
+    {
+        [HarmonyPatch(typeof(GUI_QteModalBase),"Initialize")]
+        static void Postfix(
+            GUI_QteModalBase __instance)
+        {
+            AccessTools.Method(typeof(GUI_QteModalBase), "Success").Invoke(__instance, null);
+        }
+    }
+
+    private void Awake()
+    {
+        _logger = Logger;
+        _logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        var h = new Harmony("faster_interactions");
+        h.PatchAll();
+    }
+}
